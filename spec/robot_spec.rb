6db19164cd6aa37adea652eae66f252b37499739
“nicholashibberd” <nicholashibberd@gmail.com>
2016-03-01 23:24:20 +0000

    Ignores all erroneous command

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -4,10 +4,22 @@ describe Robot do
   subject { Robot.new }
 
   describe ".respond" do
-    let(:command) { "some other command" }
-
-    it "ignores erroneous commands" do
-      expect(subject.respond(command)).to be_nil
+    describe "erroneous commands" do
+      it "ignores them" do
+        commands = [
+          "PLACE0,0,NORTH",
+          "PLACE 0,0,NORTH xxx",
+          "PLACE 5,0,NORTH",
+          "PLACE 0,0,N",
+          "xPLACE 0,0,N",
+          "Left",
+          "R",
+          "move"
+        ]
+        commands.each do |command|
+          expect(subject.respond(command)).to be_nil
+        end
+      end
     end
   end
 end
